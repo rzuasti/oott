91c5926e1b3f5b0dab1ca8875e1a6d85d167f5ba
rzuasti <rzuasti@pm.me>
2026-02-12 09:31:35 -0500

    Adding json lock file for flutter project

--- nix/package.nix
+++ nix/package.nix
@@ -1,4 +1,7 @@
-{pkgs}: let
+{
+  lib,
+  pkgs,
+}: let
   oott-backend = pkgs.rustPlatform.buildRustPackage rec {
     pname = "oott-backend";
     version = "0.0.1";
@@ -10,12 +13,25 @@
     nativeBuildInputs = [pkgs.pkg-config];
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
   };
+
+  oott-frontend = pkgs.flutter.buildFlutterApplication rec {
+    pname = "oott-frontend";
+    version = "0.0.1";
+    src = ./../frontend;
+    pubspecLock = lib.importJSON ./../frontend/pubspec.lock.json;
+
+    buildInputs = [pkgs.yj];
+    preBuild = ''
+      cat $src/pubspec.lock | yj > $src/pubspec.lock.json
+    '';
+  };
 in
   pkgs.stdenv.mkDerivation {
     name = "oott";
-    buildInputs = [oott-backend];
+    buildInputs = [oott-frontend oott-backend];
     src = ./../extras;
     installPhase = ''
+      mkdir -p $out/bin
       cp ${oott-backend}/bin/* $out/bin/
     '';
   }
